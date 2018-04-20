cc676b464158d5cbba7079867cb4fe5751a19d8f
Niklas Halonen <kahvikannu@gmail.com>
2018-04-20 07:24:55 +0300

    Ruler extension

--- src/csv/main.py
+++ src/csv/main.py
@@ -4,6 +4,7 @@ from csv import reader
 import matplotlib.pyplot as plt
 import numpy as np
 from savitzky_golay import *
+from scipy.signal import butter, lfilter, freqz
 
 filename = 'pallon pomppiminen.csv'
 with open(filename,'r') as f:
@@ -29,19 +30,44 @@ a = l[2:,3+4*run].astype(np.float)
 #                   list, window size, order
 ahat = savitzky_golay(a, 11, 1)
 
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+# Filter requirements.
+order = 3
+fs = 100.0       # sample rate, Hz
+cutoff = 40.      # desired cutoff frequency of the filter, Hz
+# Get the filter coefficients so we can check its frequency response.
+
+ahat2 = butter_lowpass_filter(a, cutoff, fs, order)
+
 fig, axes = plt.subplots(nrows=1, ncols=1)
 fig.subplots_adjust(left=0.05, bottom=0.05, right=0.98, top=0.98)
-label_text = [r"%.2f$s$"%float(loc) for loc in plt.xticks()[0]]
-axes.set_xticklabels(label_text)
+#label_text = [r"%.2f$s$"%float(loc) for loc in plt.xticks()[0]]
+#axes.set_xticklabels(label_text)
 
 plt.scatter(t, p, 4, 'r', label='{} {}'.format(
     l[0,1+4*run], l[1,1+4*run]))
 plt.scatter(t, v, 4, 'g', label='{} {}'.format(
     l[0,2+4*run], l[1,2+4*run]))
-# plt.scatter(t, a, 4, 'b', label='{} {}'.format(
-#     l[0,3+4*run], l[1,3+4*run]))
-plt.scatter(t, ahat, 4, 'darkblue', label='{} {}'.format(
+plt.scatter(t, a, 4, 'b', label='{} {}'.format(
     l[0,3+4*run], l[1,3+4*run]))
+plt.scatter(t, ahat, 4, 'm', label='{} {}'.format(
+    l[0,3+4*run], l[1,3+4*run]))
+
+import sys
+sys.path.append('matplotlib-tools/')
+from matplotlib_tools.tools import Ruler
+markerprops = dict(marker='o', markersize=5, markeredgecolor='red')
+lineprops = dict(color='red', linewidth=2)
+ruler = Ruler(ax=axes,
+    useblit=True, markerprops=markerprops, lineprops=lineprops)
 
 plt.legend()
 plt.grid()
